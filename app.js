35fdd916fb2bb420fc1312452da866959b515db7
Ethan Harris <ethanharris1105@gmail.com>
2022-02-25 14:50:00 -0800

    add https status code stuff

--- app.js
+++ app.js
@@ -165,6 +165,9 @@ app.post("/login", function (req, res) {
       passport.authenticate("local")(req, res, function () {
         res.redirect("/");
       });
+      if(res.status(400)||res.status(401)){
+        res.redirect("/login");
+      }
     }
   });
 });
